f576ddf3c62f1f6d9d556094703cf3e348d3df1c
Eigil Wretström <eigilw@student.chalmers.se>
2026-01-27 11:15:16 +0100

    Created test directory and for Volvo240

---
+++ test/Volvo240Test.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Volvo240Test {
+
+    @Test
+    public void speedFactor() {
+    }
+
+    @Test
+    public void incrementSpeed() {
+    }
+
+    @Test
+    public void decrementSpeed() {
+    }
+}
