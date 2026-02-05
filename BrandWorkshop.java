1cae06028cc300d8810ce7bc2824c0af6e7d7416
Fredrik Nilsson <nilsfre@student.chalmers.se>
2026-02-05 17:57:08 +0100

    Testing for workshop is done, and boet is done

--- BrandWorkshop.java
+++ BrandWorkshop.java
@@ -1,5 +1,30 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
 
-public class BrandWorkshop<TBrand extends Car> extends Workshop {
+public class BrandWorkshop<TBrand extends Car>{
+    private int m_carCapacity = 0;
+    protected Map<Integer, Car> m_garage = new HashMap<>();
+
+    public BrandWorkshop() {
+        this(0);
+    }
+
+    public BrandWorkshop(int carCapacity) {
+        this.m_carCapacity = carCapacity;
+    }
+
+    public int getCarCapacity() {
+        return m_carCapacity;
+    }
+
+    public int getNumOfCars() {
+        return m_garage.size();
+    }
+
+    public boolean canAcceptCar() {
+        return this.m_garage.size() < this.m_carCapacity;
+    }
 
 
     Integer acceptCar(TBrand car) {
