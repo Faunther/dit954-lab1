cd3ed314141a375f8d1c4933f038999cb9fc7eab
Eigil Wretström <eigilw@student.chalmers.se>
2026-02-28 18:20:13 +0100

    Made Overridden methods in publisher explicit and implemented Timer and IGameTickHandler

--- src/abc/Publisher.java
+++ src/abc/Publisher.java
@@ -1,11 +1,42 @@
 package src.abc;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
-public class Publisher implements IViewActionsHandler {
+public class Publisher implements IViewActionsHandler, IGameTickHandler {
     protected ArrayList<IDriveSubscriber> driveSubscribers = new ArrayList<>();
     protected ArrayList<ITurboSubscriber> turboSubscribers = new ArrayList<>();
     protected ArrayList<IRaiseLowerBedSubscriber> raiseLowerBedSubscribers = new ArrayList<>();
+    protected ArrayList<IGameTickSubscriber> m_gameTickSubscribers = new ArrayList<>();
+
+
+    // The delay (ms) corresponds to 20 updates a sec (hz)
+    private final int delay = 50;
+    // The timer is started with a listener (see below) that executes the statements
+    // each step between delays.
+    private Timer m_timer = new Timer(delay, new TimerListener());
+    private class TimerListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            // Tell all driveSubscribers to move.
+            for(IDriveSubscriber driveSub: driveSubscribers) {
+                driveSub.onMoveTickEvent();
+            }
+
+            // Implicit !!!
+            // Tell view (GameTickSubscriber) to draw.
+            for (IGameTickSubscriber gtSub: m_gameTickSubscribers) {
+                gtSub.onGameTick();
+            }
+        }
+    }
+
+    @Override
+    public void addSubscriber(IGameTickSubscriber gtSub) {
+        this.m_gameTickSubscribers.add(gtSub);
+    }
 
     public void addSubscriber(IDriveSubscriber cs) {
         this.driveSubscribers.add(cs);
@@ -18,48 +49,56 @@ public class Publisher implements IViewActionsHandler {
         }
     }
 
+    @Override
     public void onClickStartEngine() {
         for (IDriveSubscriber ds : driveSubscribers) {
             ds.onStartEngineEvent();
         }
     }
 
+    @Override
     public void onClickStopEngine() {
         for (IDriveSubscriber ds : driveSubscribers) {
             ds.onStopEngineEvent();
         }
     }
 
+    @Override
     public void onClickGas(int gasAmount) {
         for (IDriveSubscriber s : driveSubscribers) {
             s.onGasEvent(gasAmount);
         }
     }
 
+    @Override
     public void onClickBrake(int brakeEvent) {
         for (IDriveSubscriber s : driveSubscribers) {
             s.onBrakeEvent(brakeEvent);
         }
     }
 
+    @Override
     public void onClickTurboOn() {
         for (ITurboSubscriber s : turboSubscribers) {
             s.onTurboEvent(true);
         }
     }
 
+    @Override
     public void onClickTurboOff() {
         for (ITurboSubscriber s : turboSubscribers) {
             s.onTurboEvent(false);
         }
     }
 
+    @Override
     public void onClickLowerBed() {
         for (IRaiseLowerBedSubscriber s : raiseLowerBedSubscribers) {
             s.onLowerBed();
         }
     }
 
+    @Override
     public void onClickRaiseBed() {
         for (IRaiseLowerBedSubscriber s : raiseLowerBedSubscribers) {
             s.onRaiseBed();
