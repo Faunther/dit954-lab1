31b5c9b7f656f1e922cf2927819f9b05fdb78339
Eigil Wretström <eigilw@student.chalmers.se>
2026-03-03 21:17:35 +0100

    Cleaned up unnecessary comments

--- src/abc/Application.java
+++ src/abc/Application.java
@@ -11,37 +11,10 @@ public class Application {
         CarSystem<?> scaniaSys = carSysFactory.createSystem("Scania");
         CarSystem<?> volvo240Sys = carSysFactory.createSystem("Volvo240");
 
-        //Saab95System<Saab95> saabSys = new Saab95System<>();
-        //pub.addSubscriber(saabSys);
-        //saabSys.addCar(new Saab95());
-        //saabSys.addCar(new Saab95());
-        //saabSys.addCar(new Saab95());
-
-        //ScaniaSystem<Scania> scaniaSystem = new ScaniaSystem<>();
-        //pub.addSubscriber(scaniaSystem);
-        //scaniaSystem.addCar(new Scania());
-        //scaniaSystem.addCar(new Scania());
-        //scaniaSystem.addCar(new Scania());
-
         Model carModel = new Model();
         carModel.addSystem(saab95Sys);
         carModel.addSystem(scaniaSys);
         // carModel.addCarBrandWorkshop(); // handle here?
-
-        /*
-        ArrayList<CarSystem<?>> systems = new ArrayList<>();
-        systems.add(saabSys);
-        systems.add(scaniaSystem);
-
-        ArrayList<Car> outCars = new ArrayList<>();
-        for (CarSystem<?> carSystem : systems) {
-            var cars = carSystem.getCars();
-            for (Car car : outCars) {
-                String model = car.getModelName();
-                render(model, car);
-            }
-        }
-        * */
 
         // Used to store as IView
         CarView v = new CarView(carModel);
